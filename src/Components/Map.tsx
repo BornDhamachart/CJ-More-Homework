881efb9dee2e5e1b8edd5d5db01b74209e532945
BornDhamachart <dhamachart.c@gmail.com>
2023-10-31 20:11:56 +0700

    chore:update gitignore and map API KEYS

--- src/Components/Map.tsx
+++ src/Components/Map.tsx
@@ -1,9 +1,5 @@
 import React, { useState } from "react";
-import {
-  GoogleMap,
-  LoadScript,
-  MarkerF,
-} from "@react-google-maps/api";
+import { GoogleMap, LoadScript, MarkerF } from "@react-google-maps/api";
 import { ShopDetail } from "../interface";
 
 interface Props {
@@ -13,42 +9,40 @@ interface Props {
 const Map: React.FC<Props> = ({ shopDetail }) => {
   console.log("shopDetail", shopDetail);
 
-
   const containerStyle = {
     width: "50%",
     height: "50vh",
   };
 
-//   const mapOptions = {
-//     zoomControl: true,
-//     scaleControl: true,
-//     fullscreenControl: true,
-//     streetViewControl: true,
-//     mapTypeControl: true,
-//   };
+  //   const mapOptions = {
+  //     zoomControl: true,
+  //     scaleControl: true,
+  //     fullscreenControl: true,
+  //     streetViewControl: true,
+  //     mapTypeControl: true,
+  //   };
 
-const markPosition = {
-    lat : 12.979881,
-    lng: 101.194
+  const markPosition = {
+    lat: 12.979881,
+    lng: 101.194,
     // lat : shopDetail.lat,
     // lng: shopDetail.lng
-}
+  };
+
   return (
     <>
       <div className="rounded-xl overflow-auto">
-        <LoadScript googleMapsApiKey="AIzaSyDj4Xk71S-odYeKkvLLh5i1FCI5LmoHAmE">
+        <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLEMAP_API_KEY}>
           <GoogleMap
             mapContainerStyle={containerStyle}
             zoom={10}
             center={markPosition}
             // options={mapOptions}
           >
-            
-              <MarkerF
-                // key={shopDetail.code}
-                position={markPosition}
-              />
-            
+            <MarkerF
+              // key={shopDetail.code}
+              position={markPosition}
+            />
           </GoogleMap>
         </LoadScript>
       </div>
--- src/Components/ShopLayout.tsx
+++ src/Components/ShopLayout.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Popconfirm } from "antd";
 
 interface Props {
   isModalShopCreateVisible: boolean;
@@ -7,31 +8,36 @@ interface Props {
   setIsModalShopCheckVisible: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const ShopLayout : React.FC<Props> = ({
+const ShopLayout: React.FC<Props> = ({
   isModalShopCreateVisible,
   setIsModalShopCreateVisible,
   isModalShopCheckVisible,
-  setIsModalShopCheckVisible
+  setIsModalShopCheckVisible,
 }) => {
   const shelft = [];
   for (let i = 1; i <= 10; i++) {
     shelft.push(i);
   }
 
-
   return (
     <>
       <div className="flex justify-center">
         <div className="m-16 border border-gray-400 h-96 w-2/3 grid grid-cols-5 gap-8 p-8 ">
           {shelft.map((r) => (
-            <div 
-            className="border border-black flex justify-center items-center cursor-pointer hover:border-green-400"
-            onClick = {() => setIsModalShopCheckVisible(true)}
+            <Popconfirm
+              description="Please select options"
+              onConfirm={() => setIsModalShopCheckVisible(true)}
+              onCancel={() => setIsModalShopCreateVisible(true)}
+              icon={<></>}
+              okText="Check picture"
+              cancelText="Add picture"
             >
-              <div className="border border-black rounded-full w-6 text-center">
-                {r}
+              <div className="border border-black flex justify-center items-center cursor-pointer hover:border-green-400">
+                <div className="border border-black rounded-full w-6 text-center">
+                  {r}
+                </div>
               </div>
-            </div>
+            </Popconfirm>
           ))}
         </div>
       </div>
