f5943823fb53359d1567a7f9fe0fc1c945a2928d
Dhamachart <dhamachart.c@siamraj.com>
2023-11-02 15:14:37 +0700

    feat:Add add dashboard summary card homepage

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import { ShopDetail } from "../interface";
 import { MdLocationPin } from "react-icons/md";
 import Search from "../Components/Search";
-import { AiFillShop } from "react-icons/ai";
+import { AiFillShop, AiOutlineArrowUp } from "react-icons/ai";
 
 const HomePage: React.FC = () => {
   const [shopLocationData, setShopLocationData] =
@@ -25,9 +25,7 @@ const HomePage: React.FC = () => {
       );
     } else if (filters.province !== "" && filters.name === "") {
       setShopLocationData(
-        shopLocation?.filter(
-          (r) => r.province === Number(filters.province)
-        )
+        shopLocation?.filter((r) => r.province === Number(filters.province))
       );
     } else if (filters.province === "" && filters.name !== "") {
       setShopLocationData(
@@ -44,39 +42,52 @@ const HomePage: React.FC = () => {
         <div>Shops List</div>
         <AiFillShop />
       </div>
-
+      <div className="flex gap-8">
+      <div className="border border-gray-400 rounded-xl p-8">
+          <div className="text-gray-400">Total branch</div>
+          <div className="text-lg font-bold">1256</div>
+        </div>
+        <div className="border border-gray-400 rounded-xl p-8">
+          <div className="text-gray-400">Total pending branch</div>
+          <div className="text-lg font-bold">654</div>
+          <div className="flex items-center gap-1">
+            <div className="text-blue-500 text-xl">
+            <AiOutlineArrowUp />
+            </div>
+            <div>58 this week</div>
+          </div>
+        </div>
+      </div>
       <div>
         <Search setFilters={setFilters} />
       </div>
       <div className="border border-gray-200 rounded-lg p-4">
-      <div className="w-full text-right px-4 pb-2">
-        Totals : {shopLocationData?.length}
-      </div>
+        <div className="w-full text-right px-4 pb-2">
+          Totals : {shopLocationData?.length}
+        </div>
 
-      <div className="overflow-y-auto w-full h-screen grid grid-cols-3 gap-6">
-        {shopLocationData?.map((r: ShopDetail) => (
-          <Link to={`shop/${r.code}`}>
-            <div className="rounded-xl p-4 cursor-pointer hover:bg-blue-400 h-full shadow-lg">
-              <div className="flex justify-between gap-1">
-                
+        <div className="overflow-y-auto w-full h-screen grid grid-cols-3 gap-6">
+          {shopLocationData?.map((r: ShopDetail) => (
+            <Link to={`shop/${r.code}`}>
+              <div className="rounded-xl p-4 cursor-pointer hover:bg-blue-400 h-full shadow-lg">
+                <div className="flex justify-between gap-1">
                   <div className="font-bold text-lg">{r.branch}</div>
                   <div className="rounded-lg w-1/3 text-center text-sm p-1 h-1/5 text-yellow-500 border border-yellow-500">
                     Pending{"("}
                     {r.province}
                     {")"}
                   </div>
-                
-              </div>
-              <div className="flex mt-2 gap-2">
-                <div className="text-lg mt-1 text-gray-500">
-                  <MdLocationPin />
                 </div>
-                <div className="text-sm text-gray-500">{r.address}</div>
+                <div className="flex mt-2 gap-2">
+                  <div className="text-lg mt-1 text-gray-500">
+                    <MdLocationPin />
+                  </div>
+                  <div className="text-sm text-gray-500">{r.address}</div>
+                </div>
               </div>
-            </div>
-          </Link>
-        ))}
-      </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
